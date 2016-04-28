71401620a3c3c160951f3b97b9ed629c295b112f
Zakycka163 <diman.stavinskij2011@ya.ru>
2016-04-28 13:51:22 +0400

    - добален фон для регистрации и авторизации - выравнено: регистрация и авторизация (на одном уравне) - удалено внутренний бутстрап, теперь только через интернет

--- sign_in.php
+++ sign_in.php
@@ -1,47 +1,49 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <meta charset="utf-8">
-    <title>Авторизация</title>
+        <meta charset="utf-8">
+        <title>Авторизация</title>
 
 <!-- Latest compiled and minified CSS -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 <!-- Optional theme -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
 <!-- Latest compiled and minified JavaScript -->
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
         <style type="text/css">
             body {
-                padding-top: 40px;
-                padding-bottom: 40px;
-                background-color: #f5f5f5;
+                padding-top: 210px;
+                padding-bottom: 315px;
+                background: #0cabfa url(img/PVGUS_4_2.jpg) no-repeat;
+                -moz-background-size: 100%; /* Firefox 3.6+ */
+                -webkit-background-size: 100%; /* Safari 3.1+ и Chrome 4.0+ */
+                -o-background-size: 100%; /* Opera 9.6+ */
+                background-size: cover;     
             }
-
-        .form-signin {
-            max-width: 300px;
-            padding: 19px 29px 29px;
-            margin: 0 auto 20px;
-            background-color: #fff;
-            border: 1px solid #e5e5e5;
-            -webkit-border-radius: 5px;
-                -moz-border-radius: 5px;
-                    border-radius: 5px;
-            -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-                -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-                    box-shadow: 0 1px 2px rgba(0,0,0,.05);
-        }
-        .form-signin .form-signin-heading,
-        .form-signin .checkbox {
-            margin-bottom: 10px;
-        }
+            .form-signin {
+                max-width: 300px;
+                padding: 19px 29px 29px;
+                margin: 0 auto 20px;
+                background-color: #fff;
+                border: 1px solid #e5e5e5;
+                -webkit-border-radius: 5px;
+                    -moz-border-radius: 5px;
+                        border-radius: 5px;
+                -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                    -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                        box-shadow: 0 1px 2px rgba(0,0,0,.05);
+            }
+            .form-signin .form-signin-heading,
+            .form-signin .checkbox {
+                margin-bottom: 10px;
+            }
         </style>
         
     </head>
     <body>
-        <div class="container">
+        <div class="container" >
 
             <form class="form-signin">
                 <h2 class="form-signin-heading">Авторизация</h2>
@@ -55,10 +57,11 @@
                     <input type="password" class="form-control" placeholder="Пароль" aria-describedby="basic-addon1">
                 </div>
                 <br>
-                <p>
-                    <button class="btn btn-success" type="submit">Войти</button>
+                <center>
+                    <button class="btn btn-success" type="submit">Вход</button>
                     <a class="btn btn-primary" href="sign_up.php" type="button">Регистрация</a>
-                </p>
+                    <a class="btn btn-danger" href="index.php" type="button">Назад</a>
+                </center>
             </form>
         </div> <!-- /container -->
     </body>
--- sign_up.php
+++ sign_up.php
@@ -1,49 +1,52 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <meta charset="utf-8">
-    <title>Регистрация</title>
+        <meta charset="utf-8">
+        <title>Регистрация</title>
 
 <!-- Latest compiled and minified CSS -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 <!-- Optional theme -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
 <!-- Latest compiled and minified JavaScript -->
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     
         <style type="text/css">
             body {
-                padding-top: 40px;
-                padding-bottom: 40px;
-                background-color: #f5f5f5;
+                padding-top: 210px;
+                padding-bottom: 49px;
+                background: #0cabfa url(img/PVGUS_4_2.jpg) no-repeat;
+                -moz-background-size: 100%; /* Firefox 3.6+ */
+                -webkit-background-size: 100%; /* Safari 3.1+ и Chrome 4.0+ */
+                -o-background-size: 100%; /* Opera 9.6+ */
+                background-size: cover;     
             }
-
-        .form-signin {
-            max-width: 300px;
-            padding: 19px 29px 29px;
-            margin: 0 auto 20px;
-            background-color: #fff;
-            border: 1px solid #e5e5e5;
-            -webkit-border-radius: 5px;
-                -moz-border-radius: 5px;
-                    border-radius: 5px;
-            -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-                -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-                    box-shadow: 0 1px 2px rgba(0,0,0,.05);
-        }
-        .form-signin .form-signin-heading,
-        .form-signin .checkbox {
-            margin-bottom: 10px;
-        }
+            .form-signin {
+                max-width: 300px;
+                padding: 19px 29px 29px;
+                margin: 0 auto 20px;
+                background-color: #fff;
+                border: 1px solid #e5e5e5;
+                -webkit-border-radius: 5px;
+                    -moz-border-radius: 5px;
+                        border-radius: 5px;
+                -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                    -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                        box-shadow: 0 1px 2px rgba(0,0,0,.05);
+            }
+            .form-signin .form-signin-heading,
+            .form-signin .checkbox {
+                margin-bottom: 10px;
+            }
         </style>
         
     </head>
     <body>
         <div class="container">
 
-            <form class="form-signup">
+            <form class="form-signin">
                 <h2 class="form-signin-heading">Регистрация</h2>
                 <div class="input-group">
                     <label for="First">Фамилия</label>
@@ -70,9 +73,10 @@
                     <input type="password" class="form-control" id="Password">
                 </div>
                 <br>
-                <p>
+                <center>
                     <button class="btn btn-primary" type="sumit">Зарегистрироваться</button>
-                </p>
+                    <a class="btn btn-danger" href="sign_in.php" type="button">Отмена</a>
+                </center>
             </form>
         </div> <!-- /container -->
     </body>
