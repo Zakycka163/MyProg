883686fc46892a02c1d4f332892083312e23d6e0
Zakycka163 <diman.stavinskij2011@ya.ru>
2016-05-10 17:11:17 +0400

    добаление шаблона для задач

--- blocks/navAdm.php
+++ blocks/navAdm.php
@@ -17,7 +17,7 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Задачи <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="#">Смотреть</a></li>
+                        <li><a href="/MyProg/myMission.php">Смотреть</a></li>
                         <li role="separator" class="divider"></li>
                         <li><a href="#">Определить</a></li>
                         <li><a href="#">Изменить</a></li>
