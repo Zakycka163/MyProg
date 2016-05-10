01031bc693158f36775d4c2f0a6562ae38c7fd11
Zakycka163 <diman.stavinskij2011@ya.ru>
2016-05-10 17:20:12 +0400

    переименование кнопки

--- blocks/buttons/exit.php
+++ blocks/buttons/exit.php
@@ -3,6 +3,6 @@
         Дмитрий
     </li>
     <li class="myClass">
-        <input class="btn btn-danger btn-sm" onclick="location.href='/MyProg/security/sign_in.php'" type="button" value="Войти">
+        <input class="btn btn-danger btn-sm" onclick="location.href='/MyProg/security/sign_in.php'" type="button" value="Выход">
     </li>
 </ul>
