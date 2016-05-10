23f6f07061b725ba8eb6041735b699e461d981ab
Zakycka163 <diman.stavinskij2011@ya.ru>
2016-05-10 17:11:37 +0400

    шаблон мои задачи

---
+++ myMission.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Мои задачи</title>
+
+        <?php require_once "blocks/links.php" ?>
+        
+        <link href="js/bootstrap-fix.js">
+        <link href="css/bootstrap-fix.css" rel="stylesheet" type="text/css">
+        <link href="css/forSignOnIndex.css" rel="stylesheet" type="text/css">
+        <link href="css/background2.css" rel="stylesheet" type="text/css">
+        
+    </head>
+    <body>
+        <?php require_once "blocks/navAdm.php" ?>
+        <?php require_once "blocks/footer.php" ?>
+    </body>
+</html>
