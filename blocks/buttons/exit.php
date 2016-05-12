bde327aa57d23d58679025f63c8031cbf6cfe630
Zakycka163 <diman.stavinskij2011@ya.ru>
2016-05-12 23:57:23 +0400

    добавлена переменная user

--- blocks/buttons/exit.php
+++ blocks/buttons/exit.php
@@ -1,6 +1,6 @@
 <ul class="nav navbar-nav navbar-right">
     <li class="myClass2">
-        Дмитрий
+        <?php echo $user ?>
     </li>
     <li class="myClass">
         <input class="btn btn-danger btn-sm" onclick="location.href='/MyProg/security/sign_in.php'" type="button" value="Выход">
--- blocks/navWork.php
+++ blocks/navWork.php
@@ -1,3 +1,4 @@
+<?php $user="Преподаватель" ?>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
 <!-- Brand and toggle get grouped for better mobile display -->
