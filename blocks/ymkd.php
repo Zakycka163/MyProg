59a1a5c1945505d8c46360b8586104a8a58ae181
Zakycka163 <diman.stavinskij2011@ya.ru>
2016-05-17 20:06:12 +0400

    правки

--- blocks/ymkd.php
+++ blocks/ymkd.php
@@ -1,27 +1,6 @@
 <center><h3>Разработка УМКД</h3></center>
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <center>Титульный лист</center>
-    </div>
-    <div class="panel-body">
-        Panel content
-    </div>
-</div>
 
-<div class="form-group">
-    <center><label for="anot">Аннотация</label></center>
-    <input type="file" accept="application/msword" required class="form-control" id="anot">
-</div>
-
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <center>Рабочая учебная программа дисциплины</center>
-    </div>
-    <div class="panel-body">
-        <center><h5>1. Цели освоения дисциплины</h5></center>
-        <input type="text" minlength=20 class="form-control" required>
-    </div>
-</div>
+<?php require_once "blocks/workProg.php";?>
 
 <div class="panel panel-default">
     <div class="panel-heading">
@@ -33,7 +12,7 @@
 </div>
 
 <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#kyrsa4" aria-expanded="false" aria-controls="collapseExample">
-    Методические указания для выполнения работы/проекта
+    Методические указания для выполнения курсовой работы/проекта
 </button>
 <div class="collapse" id="kyrsa4">
     <div class="panel panel-default">
@@ -56,19 +35,6 @@
     </div>
 </div>
 
-<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#labs" aria-expanded="false" aria-controls="collapseExample">
-    Лабораторный практикум
-</button>
-<div class="collapse" id="labs">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <center>Лабораторный практикум</center>
-        </div>
-        <div class="panel-body">
-            Panel content
-        </div>
-    </div>
-</div>
-
+<?php require_once "blocks/labPrak.php";?>
 
 
