98c786b45ba25e246ec5eeb5dfd2e6192729801f
Zakycka163 <diman.stavinskij2011@ya.ru>
2016-05-17 20:05:02 +0400

    библиотека phpword

---
+++ blocks/labPrak.php
@@ -0,0 +1,13 @@
+<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#labs" aria-expanded="false" aria-controls="collapseExample">
+    Лабораторный практикум
+</button>
+<div class="collapse" id="labs">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <center>Лабораторный практикум</center>
+        </div>
+        <div class="panel-body">
+            Panel content
+        </div>
+    </div>
+</div>
---
+++ blocks/workProg.php
@@ -0,0 +1,23 @@
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <center>Титульный лист</center>
+    </div>
+    <div class="panel-body">
+        Panel content
+    </div>
+</div>
+
+<div class="form-group">
+    <center><label for="anot">Аннотация</label></center>
+    <input type="file" accept="application/msword" required class="form-control" id="anot">
+</div>
+
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <center>Рабочая учебная программа дисциплины</center>
+    </div>
+    <div class="panel-body">
+        <center><h5>1. Цели освоения дисциплины</h5></center>
+        <input type="textbox" minlength=30 class="form-control" required>
+    </div>
+</div>
