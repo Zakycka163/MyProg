01749bf865568332635e42b692eac44a8ef5b677
Zakycka163 <diman.stavinskij2011@ya.ru>
2016-06-10 03:36:49 +0400

    умр

--- pages/ymr.php
+++ pages/ymr.php
@@ -39,6 +39,15 @@
                     </div>
                     
                     <div class="form-group">
+                        <label for="InputType">Группа</label><br>
+                        <select id="InputType" name="task">
+                            <option selected disabled>Выбрать группу</option>
+                            <option task="1">Задача 1</option>
+                            <option task="2">Задача 2</option>
+                        </select><br>
+                    </div>
+                    
+                    <div class="form-group">
                         <label for="InputName">Название разработки</label>
                         <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
                     </div>
