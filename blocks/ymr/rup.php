5ec3da6fbe6cc4a05d142ee93bffa74093242086
Stavinsky Dmitry <Stavinsky Dmitry>
2016-06-13 14:58:41 +0400

    структура РУП

--- blocks/ymr/rup.php
+++ blocks/ymr/rup.php
@@ -28,7 +28,122 @@
     </div>
     
     <div class="form-group">
-        <label for="InputName">Название разработки</label>
+        <label for="InputName">1. Цели освоения дисциплины (модуля)</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">2. Место дисциплины в структуре ООП направления подготовки (специальности)</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">3. Компетенции обучающегося, формируемые в результате освоения дисциплины (модуля)</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">4. Структура и содержание дисциплины (модуля)</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">4.1. Структура дисциплины</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">4.2. Содержание дисциплины</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">4.3. Тематический план изучения дисциплины</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">4.3.1. Лекционные вопросы</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+        <div class="form-group">
+        <label for="InputName">4.3.2. Практические (семинарские) занятиям</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">4.3.3. Лабораторные занятия</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">4.3.4. Самостоятельная работа</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">5. Образовательные технологии</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">6. Оценочные средства для текущего контроля успеваемости, промежуточной аттестации по итогам освоения дисциплины (модуля)</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">7. Учебно-методическое и информационное обеспечение дисциплины (модуля)</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">7.1. Нормативно-правовые документы (нормативно-техническая документация)</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">7.2. Основная литература</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">7.3. Дополнительная литература</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">7.4. Периодическая литература</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+     <div class="form-group">
+        <label for="InputName">7.5. Программное обеспечение современных информационно-коммуникационных технологий и Интернет-ресурсы</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">8. Материально-техническое обеспечение дисциплины</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">9. Методические указания для выполнения контрольных работ</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">10. Протокол согласования с другими дисциплинами ООП</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">11. Примерная технологическая карта дисциплины (по семестрам)</label>
+        <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <label for="InputName">12. Дополнения и изменения к рабочей учебной программе</label>
         <textarea type="text" required class="form-control" id="InputName" name="Name"></textarea>
     </div>
     
