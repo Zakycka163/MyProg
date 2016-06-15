4e5b924a107a74effbb9b29d9158fda72e2f109d
Zakycka163 <diman.stavinskij2011@ya.ru>
2016-06-16 00:52:54 +0400

    ver1.9

--- pages/myMission.php
+++ pages/myMission.php
@@ -32,6 +32,19 @@
                 } else {
                     print "Задачи отсутствуют!";
                 }
+        echo "<br><center><h3>Возвраты</h3></center>";
+                $result = mysqli_query($link, "SELECT w.name, r.comment, r.return_date, u.first_name, u.second_name FROM returns r, users u, works w, tasks t WHERE t.user_id='".$id."' AND t.task_id=w.task_id AND r.work_id=w.work_id AND u.user_id=r.user_id");
+                if($result){
+                    echo "<table class='table table-bordered'><tr class='active'><td>№</td><td>УМР</td><td>Замечание</td><td>Дата возврата</td><td>Автор возврата</td></tr>";
+                    $i = 1; 
+                    while($row = mysqli_fetch_row($result)){
+                        echo "<tr><td>".$i."</td><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]." ".$row[4]."</td></tr>";
+                        $i++;
+                    }
+                    echo "</table>";
+                } else {
+                    print "Возвраты отсутствуют!";
+                }
                 close();
             ?>
         
